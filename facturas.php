f85682b5ebc658bd3b3a400dcece229ca1e8865e
Patty <patty2.4@hotmail.com>
2018-12-11 22:45:12 -0500

    Commit

--- facturas.php
+++ facturas.php
@@ -5,17 +5,17 @@
         <title>CRUD Productos</title>
     </head>
     <body>
-    <p align="center"><b><font face="Monotype Corsiva" size="6">Facturas</font></b></p>
+        <p align="center"><b><font face="Monotype Corsiva" size="6">Facturas</font></b></p>
         <table align="center">
             <tr><td>
                     <form action="./controller/controller.php">
-                        <input type="hidden" value="listar" name="opcion">
+                        <input type="hidden" value="listarF" name="opcion">
                         <input type="submit" style="width: 150px; height: 60px;" value="Ver facturas">
                     </form>
                 </td>
                 <td>
                     <form action="./controller/controller.php">
-                        <input type="hidden" value="crear" name="opcion">
+                        <input type="hidden" value="crearF" name="opcion">
                         <input type="submit" style="width: 150px; height: 60px;" value="Crear Factura">
                     </form>
                 </td></tr>
@@ -27,11 +27,11 @@
                 <th><font color="#FFFFFF">FECHA</font></th>
                 <th><font color="#FFFFFF">TOTAL</font></th>
                 <th><font color="#FFFFFF">ELIMINAR</font></th>
-                <th><font color="#FFFFFF">ACTUALIZAR</font></th>
+
             </tr>
             <?php
             session_start();
-           include './model/Factura.php';
+            include './model/Factura.php';
 //verificamos si existe en sesion el listado de productos:
             if (isset($_SESSION['listadofac'])) {
                 $listado = unserialize($_SESSION['listadofac']);
@@ -43,8 +43,8 @@
                     echo "<td>" . $prod->getApellidos() . "</td>";
 //opciones para invocar al controlador indicando la opcion eliminar o cargar
 //y la fila que selecciono el usuario (con el codigo del producto):
-                    echo "<td><a href='./controller/controllerfacturas.php?opcion=eliminar&codigo=" . $prod->getId() . "'>eliminar</a></td>";
-                    echo "<td><a href='./controller/controllerfacturas.php?opcion=cargar&codigo=" . $prod->getId() . "'>actualizar</a></td>";
+                    echo "<td><a href='./controller/controller.php?opcion=eliminarF&id=" . $prod->getId() . "'>eliminar</a></td>";
+
                     echo "</tr>";
                 }
             } else {
@@ -52,7 +52,7 @@
             }
             ?>
         </table>
-       
+
         </font>
     </body>
 </html>
